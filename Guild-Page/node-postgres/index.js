3dbaa4635321b332cfcbb347fe2704da95bb2a90
Tarsolan <alex.ridgeley@keyin.com>
2022-07-15 14:02:39 -0230

    uploading gitIgnore

--- Guild-Page/node-postgres/index.js
+++ Guild-Page/node-postgres/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3001;
-global.DEBUG = false;
+global.DEBUG = true;
 
 app.use(express.json());
 app.use(function (req, res, next) {
