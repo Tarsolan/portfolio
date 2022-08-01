41c04f8a968d18cc171f63fb074737099795b78a
Tarsolan <alex.ridgeley@keyin.com>
2022-08-01 10:51:05 -0230

    mission report display member

--- Guild-Page/client/src/Components/Missions/MissionDetail.jsx
+++ Guild-Page/client/src/Components/Missions/MissionDetail.jsx
@@ -4,148 +4,162 @@ import stamp from "../../pictures/complete_2.png";
 import pushPin from "../../pictures/push_pin.png";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import MemberInfo from "../Members/MemberInfo";
 
 const MissionDetail = ({
-  mission,
-  members,
-  loginCheck,
-  toast,
-  clientCheck,
-  clientInfo,
+	mission,
+	members,
+	handleSelectMem,
+	loginCheck,
+	toast,
+	clientCheck,
+	clientInfo,
+	selectedMember,
 }) => {
-  const {
-    complete,
-    mission_num,
-    job_name,
-    contact_name,
-    job_description,
-    payout,
-    organization,
-    deadline_date,
-    reports,
-  } = mission;
+	const {
+		complete,
+		mission_num,
+		job_name,
+		contact_name,
+		job_description,
+		payout,
+		organization,
+		deadline_date,
+		reports,
+	} = mission;
 
-  const deadline = new Date(deadline_date);
+	const deadline = new Date(deadline_date);
 
-  const [viewMission, setViewMission] = useState(true);
+	const [viewMission, setViewMission] = useState(true);
+	const [showMem, setShowMem] = useState(false);
 
-  const displayReport = () => {
-    return (
-      <div className={styles.reportContainer}>
-        {reports.map((report) => {
-          return (
-            <div className={styles.missionReport}>
-              <h3>{report.name}</h3>
-              <p>{report.report_details}</p>
-            </div>
-          );
-        })}
-        ;
-      </div>
-    );
-  };
+	const loadMem = (member_id) => {
+		handleSelectMem(member_id);
+		setShowMem(true);
+	};
 
-  const navigate = useNavigate();
-  const goToFileReport = () => {
-    if (loginCheck) {
-      navigate(`/missions/info/${mission_num}/reports/new`);
-    } else {
-      toast("Please log in as a member before filing a report.", "error");
-    }
-  };
+	const displayReport = () => {
+		return (
+			<div className={styles.reportContainer}>
+				{reports.map((report) => {
+					return (
+						<div className={styles.missionReport}>
+							<h3 onClick={() => loadMem(report)}>{report.name}</h3>
+							<p>{report.report_details}</p>
+						</div>
+					);
+				})}
+				;
+			</div>
+		);
+	};
 
-  const goToEditMission = () => {
-    if (!clientCheck) {
-      toast("Please log in as the client before editing the mission.", "error");
-      return;
-    }
-    if (clientInfo.organization !== organization) {
-      toast(`Sorry, but only ${organization} can edit this mission.`, "error");
-    } else {
-      navigate(`/missions/info/${mission_num}/edit`);
-    }
-  };
+	const navigate = useNavigate();
+	const goToFileReport = () => {
+		if (loginCheck) {
+			navigate(`/missions/info/${mission_num}/reports/new`);
+		} else {
+			toast("Please log in as a member before filing a report.", "error");
+		}
+	};
 
-  return (
-    <div id={styles.missionDetail}>
-      <div id={styles.missionPage}>
-        <div className={styles.detailTop}>
-          <img
-            src={pushPin}
-            alt="A push pin to hold paper"
-            className={styles.missionPin}
-          />
-          <h1>{job_name}</h1>
-          {viewMission ? (
-            <>
-              <div className={styles.detailRow}>
-                <span>Organization:</span>
-                <span>{organization}</span>
-              </div>
-              <div className={styles.detailRow}>
-                <span>Contact Name:</span>
-                <span>{contact_name}</span>
-              </div>
+	const goToEditMission = () => {
+		if (!clientCheck) {
+			toast("Please log in as the client before editing the mission.", "error");
+			return;
+		}
+		if (clientInfo.organization !== organization) {
+			toast(`Sorry, but only ${organization} can edit this mission.`, "error");
+		} else {
+			navigate(`/missions/info/${mission_num}/edit`);
+		}
+	};
 
-              <hr />
-              <div className={styles.detailMid}>
-                <h2>Job Description</h2>
-                <p className={styles.description}>{job_description}</p>
-                {complete && (
-                  <img
-                    src={stamp}
-                    alt="Completed mission"
-                    className={styles.missionComplete}
-                  />
-                )}
-              </div>
-              <hr />
-              <div className={styles.detailBot}>
-                <div className={styles.detailPayout}>
-                  <h3>Payout:</h3>
-                  <ul>
-                    <li>{payout} rocks</li>
-                  </ul>
-                </div>
+	return (
+		<>
+			<div id={styles.missionDetail}>
+				<div id={styles.missionPage}>
+					<div className={styles.detailTop}>
+						<img
+							src={pushPin}
+							alt="A push pin to hold paper"
+							className={styles.missionPin}
+						/>
+						<h1>{job_name}</h1>
+						{viewMission ? (
+							<>
+								<div className={styles.detailRow}>
+									<span>Organization:</span>
+									<span>{organization}</span>
+								</div>
+								<div className={styles.detailRow}>
+									<span>Contact Name:</span>
+									<span>{contact_name}</span>
+								</div>
 
-                <div className={styles.missionNumContainer}>
-                  <span id={styles.missionNum}>{mission_num}</span>
-                  <span className={styles.missionNum}>Mission Number</span>
-                </div>
-                <div className={styles.deadline}>
-                  <h3>Deadline:</h3>
-                  <p className={styles.date}>{deadline.toLocaleDateString()}</p>
-                </div>
-                <div className={styles.signatureContainer}>
-                  <span id={styles.signature}>Auron Warfield</span>
-                  <span className={styles.signature}>
-                    Guild Registrar Signature
-                  </span>
-                </div>
-              </div>
-            </>
-          ) : (
-            <>
-              <h2 className={styles.reportTitle}>Mission Reports</h2>
-              <hr />
-              <div>
-                {reports.length > 0 ? displayReport() : `No reports to show.`}
-              </div>
-            </>
-          )}
-          <hr />
+								<hr />
+								<div className={styles.detailMid}>
+									<h2>Job Description</h2>
+									<p className={styles.description}>{job_description}</p>
+									{complete && (
+										<img
+											src={stamp}
+											alt="Completed mission"
+											className={styles.missionComplete}
+										/>
+									)}
+								</div>
+								<hr />
+								<div className={styles.detailBot}>
+									<div className={styles.detailPayout}>
+										<h3>Payout:</h3>
+										<ul>
+											<li>{payout} rocks</li>
+										</ul>
+									</div>
 
-          <div className={styles.detailButtons}>
-            <button onClick={goToEditMission}>Edit Mission</button>
-            <button onClick={goToFileReport}>File Report</button>
-            <button onClick={() => setViewMission(!viewMission)}>
-              {viewMission ? `View Reports` : `View Mission Details`}
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+									<div className={styles.missionNumContainer}>
+										<span id={styles.missionNum}>{mission_num}</span>
+										<span className={styles.missionNum}>Mission Number</span>
+									</div>
+									<div className={styles.deadline}>
+										<h3>Deadline:</h3>
+										<p className={styles.date}>
+											{deadline.toLocaleDateString()}
+										</p>
+									</div>
+									<div className={styles.signatureContainer}>
+										<span id={styles.signature}>Auron Warfield</span>
+										<span className={styles.signature}>
+											Guild Registrar Signature
+										</span>
+									</div>
+								</div>
+							</>
+						) : (
+							<>
+								<h2 className={styles.reportTitle}>Mission Reports</h2>
+								<hr />
+								<div>
+									{reports.length > 0 ? displayReport() : `No reports to show.`}
+								</div>
+							</>
+						)}
+						<hr />
+
+						<div className={styles.detailButtons}>
+							<button onClick={goToEditMission}>Edit Mission</button>
+							<button onClick={goToFileReport}>File Report</button>
+							<button onClick={() => setViewMission(!viewMission)}>
+								{viewMission ? `View Reports` : `View Mission Details`}
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			{showMem && <MemberInfo member={selectedMember} />}
+		</>
+	);
 };
 
 export default MissionDetail;
